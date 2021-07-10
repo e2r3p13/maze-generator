4b2924e196a5a88911dfa6578b9085f0dd8796a0
lfalkau <lfalkau@student.42.fr>
2021-07-10 07:11:01 +0200

    factorize, create export method

--- src/main.rs
+++ src/main.rs
@@ -6,39 +6,9 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 23:06:40 by lfalkau           #+#    #+#             */
-/*   Updated: 2021/07/10 06:19:32 by lfalkau          ###   ########.fr       */
+/*   Updated: 2021/07/10 06:59:50 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// use std::env;
-
-// fn main() {
-//
-// 	let args: Vec<String>;
-// 	let width: u32;
-// 	let height: u32;
-//
-// 	args = env::args().collect();
-//     if args.len() != 3 {
-// 		println!("Usage: ./mazegen height width");
-// 		return ();
-// 	}
-// 	if let Ok(h) = args[1].parse() {
-// 		height = h;
-// 	} else {
-// 		height = 32;
-// 		println!("Error: '{}' is not a valid height", args[1]);
-// 	}
-// 	if let Ok(w) = args[2].parse() {
-// 		width = w;
-// 	} else {
-// 		width = 32;
-// 		println!("Error: '{}' is not a valid width", args[2]);
-// 	}
-//
-// 	println!("{} {}", height, width);
-// 	return ();
-// }
 
 mod maze;
 use maze::Maze;
@@ -50,4 +20,5 @@ fn main() {
 	let maze = Maze::generate(height, width);
 
 	maze.print(' ', '█');
+	maze.export("mymaze.png", 32, 3);
 }
--- src/maze.rs
+++ src/maze.rs
@@ -6,14 +6,15 @@
 /*   By: lfalkau <lfalkau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 21:35:15 by lfalkau           #+#    #+#             */
-/*   Updated: 2021/07/10 06:24:54 by lfalkau          ###   ########.fr       */
+/*   Updated: 2021/07/10 07:10:03 by lfalkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 use rand::seq::SliceRandom;
 use rand::{Rng, thread_rng};
 
-#[derive(Debug)]
+use image::{ImageBuffer, Rgb};
+
 struct Transition {
 	is_reachable: bool,
 	destination: Option<u32>,
@@ -30,7 +31,6 @@ impl Transition {
 
 }
 
-#[derive(Debug)]
 struct Cell {
 	left: Transition,
 	right: Transition,
@@ -52,7 +52,6 @@ impl Cell {
 
 }
 
-#[derive(Debug)]
 pub struct Maze {
 	height: u32,
 	width: u32,
@@ -90,9 +89,9 @@ impl Maze {
 		let mut rng = rand::thread_rng();
 		let cell_id = rng.gen_range(0..maze.cells.len());
 
-		carve_passage(&mut maze, &mut visited, cell_id, cell_id as u32);
+		carve_passage(&mut maze, &mut visited, cell_id);
 
-		maze
+		return maze;
 	}
 
 	pub fn print(&self, free_char: char, wall_char: char) {
@@ -127,24 +126,29 @@ impl Maze {
 		}
 	}
 
+	pub fn export(&self, filename: &str, ppc: u32, linewidth: u32) {
+
+		if ppc < 5 || linewidth >= ppc / 2 {
+			println!("Error: Resolution too low");
+			return ;
+		}
+		let w = self.width * ppc;
+		let h = self.height * ppc;
+
+		let mut image = ImageBuffer::<Rgb<u8>, Vec<u8> >::new(w, h);
+
+		for cell in &self.cells {
+			// TODO
+		}
+
+		image.save(filename).unwrap();
+	}
+
 }
 
-fn carve_passage(maze: &mut Maze, visited: &mut Vec<bool>, cell_id: usize, from: u32) {
+fn carve_passage(maze: &mut Maze, visited: &mut Vec<bool>, cell_id: usize) {
 
 	visited[cell_id] = true;
-
-	if maze.cells[cell_id].left.destination == Some(from) {
-		maze.cells[cell_id].left.is_reachable = true;
-	}
-	if maze.cells[cell_id].right.destination == Some(from) {
-		maze.cells[cell_id].right.is_reachable = true;
-	}
-	if maze.cells[cell_id].top.destination == Some(from) {
-		maze.cells[cell_id].top.is_reachable = true;
-	}
-	if maze.cells[cell_id].bottom.destination == Some(from) {
-		maze.cells[cell_id].bottom.is_reachable = true;
-	}
 
 	let mut directions = [0, 1, 2, 3]; // TOP RIGHT BOTTOM LEFT
 
@@ -161,7 +165,13 @@ fn carve_passage(maze: &mut Maze, visited: &mut Vec<bool>, cell_id: usize, from:
 		if let Some(id) = transition.destination {
 			if visited[id as usize] == false {
 				transition.is_reachable = true;
-				carve_passage(maze, visited, id as usize, cell_id as u32);
+				match direction {
+					0 => {maze.cells[id as usize].bottom.is_reachable = true},
+					1 => {maze.cells[id as usize].left.is_reachable = true},
+					2 => {maze.cells[id as usize].top.is_reachable = true},
+					_ => {maze.cells[id as usize].right.is_reachable = true},
+				}
+				carve_passage(maze, visited, id as usize);
 			}
 		}
 	}
